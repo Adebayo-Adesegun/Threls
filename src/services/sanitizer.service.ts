150a9181ed86eed596ab2e4b4f16d165184c224b
adesegun.adebayo <dadebayo.adesegun@gmail.com>
2025-03-26 05:38:30 +0100

    chore(sanitizer): extend sanitizer service to properly handle nested objects

--- src/services/sanitizer.service.ts
+++ src/services/sanitizer.service.ts
@@ -1,14 +1,41 @@
 import _ from 'lodash';
+import mongoose from 'mongoose';
 
 class SanitizerService {
     private toSnakeCase(obj: unknown): unknown {
         if (Array.isArray(obj)) {
             return obj.map((item) => this.toSnakeCase(item));
         }
-        if (obj !== null && typeof obj === 'object') {
+        if (obj === null || obj === undefined) {
+            return obj;
+        }
+        if (
+            typeof obj === 'boolean' ||
+            typeof obj === 'number' ||
+            typeof obj === 'string'
+        ) {
+            return obj;
+        }
+        if (mongoose.isValidObjectId(obj)) {
+            return obj.toString();
+        }
+        if (obj instanceof Date) {
+            return obj.toISOString();
+        }
+        if (Buffer.isBuffer(obj)) {
+            return obj.toString('base64');
+        }
+        if (obj instanceof Map) {
+            return Object.fromEntries(obj.entries());
+        }
+        if (obj instanceof Set) {
+            return Array.from(obj);
+        }
+        if (typeof obj === 'object') {
             const sanitizedObj = _.omit(obj, '__v');
-            return _.mapKeys(sanitizedObj, (_value: unknown, key: string) =>
-                _.snakeCase(key),
+            return _.mapValues(
+                _.mapKeys(sanitizedObj, (_value, key) => _.snakeCase(key)),
+                (value) => this.toSnakeCase(value),
             );
         }
         return obj;
